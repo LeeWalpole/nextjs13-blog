1047480e78d6b1f396f182575a82376ddf60fb3c
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-01-22 17:51:40 +0700

    simplified

--- app/page.jsx
+++ app/page.jsx
@@ -2,27 +2,19 @@ import BlogCard from "../components/BlogCard";
 import styles from "./styles.module.css";
 
 async function getPosts() {
-  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  const response = await fetch("https://dummyjson.com/posts?limit=3");
   return response.json();
 }
 
-async function getUsers() {
-  const response = await fetch("https://dummyjson.com/users?limit=10");
-  return response.json();
-}
 
 export default async function Home() {
   const { posts } = await getPosts();
-  const { users } = await getUsers();
-
-  const zipped = posts.map((post, index) => {
-    return { ...post, ...users[index] };
-  });
 
   return (
     <div className={styles.blogsContainer}>
-      {zipped.map((post) => (
-        <BlogCard key={post.id} {...post} />
+      {posts.map((post) => (
+        // <BlogCard key={post.id} {...post} />
+        <p key={post.id}> {post.title}</p>
       ))}
     </div>
   );
