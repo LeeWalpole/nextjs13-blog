c773f7207808538d32bd95e4d3a9787dcc19a333
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-01-22 19:05:46 +0700

    Got this working for Wordpress (finally)

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,101 @@
-import BlogCard from "../components/BlogCard";
-import styles from "./styles.module.css";
 
+{/* @ts-expect-error Server Component */}
+async function getData() {
+  const res = await fetch('https://www.flames.agency/api/profiles.json');
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const profiles = await getData();
+
+  return <main>
+    <div>
+      {profiles.map((profile) => (
+        <div key={profile.id}>
+          <h2>{profile.display_name}</h2>
+          <p>{profile.bio}</p>
+          <img src={profile.avatar_url} alt={profile.avatar_alt} />
+          <h3>Gallery</h3>
+          <ul>
+            {profile.gallery.map((image) => (
+              <li key={image.url_full}>
+                <img src={image.url_thumbnail} alt={image.alt} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+
+  </main>;
+}
+
+
+
+
+/*
 async function getPosts() {
-  const response = await fetch("https://dummyjson.com/posts?limit=3");
+  const response = await fetch("https://www.flames.agency/api/profiles.json");
   return response.json();
 }
 
+export default async function Home() {
+  const { profiles } = await getPosts();
+
+  return (
+    <div>
+      {profiles.map((profile) => (
+        <div key={profile.id}>
+          <h2>{profile.display_name}</h2>
+          <p>{profile.bio}</p>
+          <img src={profile.avatar_url} alt={profile.avatar_alt} />
+          <h3>Gallery</h3>
+          <ul>
+            {profile.gallery.map((image) => (
+              <li key={image.url_full}>
+                <img src={image.url_thumbnail} alt={image.alt} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+
+  );
+}
+*/
+
+
+
+
+
+
+
+/*
+// WORKING: This Works
+async function getPosts() {
+  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  return response.json();
+}
 
 export default async function Home() {
   const { posts } = await getPosts();
 
   return (
-    <div className={styles.blogsContainer}>
+    <div>
       {posts.map((post) => (
-        // <BlogCard key={post.id} {...post} />
-        <p key={post.id}> {post.title}</p>
+        <p key={post.id}>{post.id}</p> 
       ))}
     </div>
   );
 }
+*/
