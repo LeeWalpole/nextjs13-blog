e77562a02e1792ee977bd9d6b19d74bfb4e36365
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-01-22 17:15:53 +0700

    First commit (pulling in dummy data)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,15 @@
-import styles from "./styles.module.css";
-import "../styles/global.css";
-import { Inter } from "@next/font/google";
+// import styles from "./styles.module.css";
+// import "../styles/global.css";
+// import { Inter } from "@next/font/google";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-const interFont = Inter();
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={interFont.className}>
-      <body className={styles.container}>
+    <html lang="en">
+      <body>
         <Header />
         {children}
         <Footer />
